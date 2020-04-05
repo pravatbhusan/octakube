e1a1719235457cb6c8d8fac18a5cf675a2afa4bd
pravatbhusan <pravat.bhusan@gmail.com>
2020-04-05 18:42:37 +0530

    Modified shell script

--- testscript.sh
+++ testscript.sh
@@ -3,4 +3,5 @@ echo "This is First Number $FIRSTNUMBER"
 echo "This is Second Number $SECONDNUMBER"
 echo "Pass status $STATUS"
 NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
-echo "$NUM"
+echo "$NUM
+echo "modified the script"
