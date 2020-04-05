179f40796b51d58b2bc37965e0596c90f09d17fb
pravatbhusan <pravat.bhusan@gmail.com>
2020-04-05 19:09:39 +0530

    script error fixed with inverted commas

--- testscript.sh
+++ testscript.sh
@@ -3,6 +3,6 @@ echo "This is First Number $FIRSTNUMBER"
 echo "This is Second Number $SECONDNUMBER"
 echo "Pass status $STATUS"
 NUM=$(($FIRSTNUMBER + $SECONDNUMBER))
-echo "$NUM
+echo "$NUM"
 echo "modified the script"
 echo "modified the script second time"
